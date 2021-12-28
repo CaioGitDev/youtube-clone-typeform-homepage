f117927400d8655f091739b95ad1a16635092925
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-12-28 00:15:51 +0000

    implement header and main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
+import { Main, Section, Sticky } from './styles';
+import GlobalStyles from './styles/Global';
 
 const App: React.FC = () => {
-  return <h1>Hello</h1>
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
+
+  return (
+    <>
+      <GlobalStyles />
+
+      <Header />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+      </Main>
+    </>
+  )
 }
 
 
